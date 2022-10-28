535bc05e0c3e5cea1d14018e8b785f0ffff5987b
Bill Nguyen <thuan@revu.net>
2022-10-28 14:46:57 +0700

    test

---
+++ middleware.js
@@ -0,0 +1,28 @@
+import { NextResponse } from 'next/server';
+
+// The country to block from accessing the secret page
+const BLOCKED_COUNTRY = 'US';
+
+// Trigger this middleware to run on the `/secret-page` route
+export const config = {
+    matcher: '/secret-page',
+};
+
+export function middleware(req) {
+    // Extract country. Default to US if not found.
+    const country = (req.geo && req.geo.country) || 'US';
+
+    console.log(req.geo);
+
+    console.log(`Visitor from ${country}`);
+
+    // Specify the correct route based on the requests location
+    if (country === BLOCKED_COUNTRY) {
+        req.nextUrl.pathname = '/login';
+    } else {
+        req.nextUrl.pathname = `/secret-page`;
+    }
+
+    // Rewrite to URL
+    return NextResponse.rewrite(req.nextUrl);
+}
