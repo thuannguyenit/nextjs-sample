8227d41615e6311879a9ffda5b18f82e8b53b12d
Bill Nguyen <thuan@revu.net>
2022-10-31 16:24:49 +0700

    update

--- pages/secret-page.js
+++ pages/secret-page.js
@@ -2,7 +2,7 @@ export default function SecretPage() {
     return (
         <div>
             <h1>GEO Page</h1>
-            <p>This is a page used to test GEO function!</p>
+            <p>This is a secret page!</p>
         </div>
     );
 }
