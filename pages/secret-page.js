e4989ce075f5ebe5b39bfec33d193221ca455fe2
Thuan Nguyen {Bill} <thuannguyenit@gmail.com>
2022-11-26 00:11:39 +0700

    Update secret-page.js

--- pages/secret-page.js
+++ pages/secret-page.js
@@ -1,8 +1,16 @@
-export default function SecretPage() {
-    return (
-        <div>
-            <h1>GEO Page</h1>
-            <p>This is a secret page!</p>
-        </div>
-    );
+import React, { Component, useEffect } from "react";
+
+function SecretPage() {
+  useEffect(() => {
+    window.location.href = "https://google.com/about";
+  }, []);
+
+  return (
+    <div>
+      <h1>This page is not available</h1>
+      <p>You are redirecting to google.com/about</p>
+    </div>
+  );
 }
+
+export default SecretPage;
